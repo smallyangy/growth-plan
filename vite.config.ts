468f3aa6d9b3a36ede3e72c000cd7dc685ace123
smallyangy <315862477@qq.com>
2025-10-09 14:42:11 +0800

    feat: 优化部署脚本

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import uni from "@dcloudio/vite-plugin-uni";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [uni()],
+  base: '/growth-plan/', // 添加这一行，设置正确的基础路径
   server: {
     port: 8092,
     host: true
